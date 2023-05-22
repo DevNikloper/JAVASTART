e4c9ff9036a1991a12f5b02623e2e9f498e15109
DevNikloper <nikbro4091@gmail.com>
2023-05-22 21:10:38 +0530

    multiplication table of 2

---
+++ loop.java
@@ -0,0 +1,10 @@
+public class loop{
+    public static void main(String []args){
+         int i=1,n=2;
+         System.out.println("multiplication of table of 2");
+         for (i=1;i<=10;i++){
+            System.out.println(n+" "+"x"+" "+i+" "+" ="+(n*i) );
+         }
+    
+}
+}
