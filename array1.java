b8eef75be98e6a4ec6e37d4c2b5274548da9a03e
DevNikloper <nikbro4091@gmail.com>
2023-06-18 12:35:59 +0530

    explanation of arrays

---
+++ array1.java
@@ -0,0 +1,10 @@
+public class array1 {
+    public static void main(String []args){
+        int num[] ={1,2,3,4};
+        int a=0;
+        System.out.println(num);
+        System.out.println(num[0]);
+        System.out.println(a);
+    }
+    
+}
