bf4c04372d13f7f0e0270f12fc50a03e8527fd41
DevNikloper <nikbro4091@gmail.com>
2023-06-22 12:56:59 +0530

    mutable and immutable string

---
+++ string1.java
@@ -0,0 +1,13 @@
+public class string1 {
+    public static void main(String[]args){
+        String brand="Nikhil";
+        System.out.println(brand);
+        brand.concat("sahoo");
+        System.out.println(brand);
+        StringBuilder brand1=new StringBuilder("Nikhil");
+        brand1.append("Sahoo");
+        System.out.println(brand1);
+
+    }
+    
+}
