231fa711935b3a108a99a7c896988f4a60951c03
DevNikloper <nikbro4091@gmail.com>
2023-06-25 08:16:12 +0530

    "concatenation of strings"

---
+++ concatstr.java
@@ -0,0 +1,13 @@
+public class concatstr {
+    public static void main(String[]args){
+        String s1="pw";
+        String s2=s1.concat("skill");
+        String s3=new String("pwskil");
+        s3=s3.concat("skill");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+    }
+    
+}
